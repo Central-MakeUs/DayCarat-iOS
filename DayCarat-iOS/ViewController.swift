d0858d817bffed85b1286ca2533b64bab779e0db
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2024-01-04 23:47:53 +0900

    Initial Commit

---
+++ DayCarat-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DayCarat-iOS
+//
+//  Created by 최지철 on 1/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
