40958da4c5a0892561bf9535f3af57ae1eea7d22
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2024-01-07 13:42:32 +0900

    add: 폰트파일추가

--- DayCarat-iOS/Source/Present/OnBoarding/Agree/AgreeViewController.swift
+++ DayCarat-iOS/Source/Present/OnBoarding/Agree/AgreeViewController.swift
@@ -7,23 +7,11 @@
 
 import UIKit
 
-class AgreeViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+class AgreeViewController: BaseViewController {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    private let titleLabel = DayCaratLabel(type: .Header6, text: "약관에 동의해주세요", textColor: .black)
+    private let desLabel = DayCaratLabel(type: .Body2, text: "어쩌구저쩌구\n설명", textColor: .gray500)
+    
+    
 }
+
